eb94bd67bb5da3ec2b6a0ca0a945f6c191fadb15
bryce0110 <bryce0110@gmail.com>
2026-03-14 14:49:23 -0500

    Quest interface

--- src/edu/mu/Quest.java
+++ src/edu/mu/Quest.java
@@ -1,5 +1,9 @@
 package edu.mu;
 
 public interface Quest {
-
+	public int getId();
+	public String getTitle();
+	public int getBasePoints();
+	public boolean isCompleted();
+	public int completeFor(Student s);
 }
