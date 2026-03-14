24ca45894f52635b2891c5ccacc8ba56527f5c33
Connor Bailey <connorwb2005@gmail.com>
2026-03-14 17:41:22 -0500

    Implemented StreakQuest

--- src/edu/mu/StreakQuest.java
+++ src/edu/mu/StreakQuest.java
@@ -1,5 +1,29 @@
 package edu.mu;
 
-public class StreakQuest {
+public class StreakQuest extends AbstractQuest{
 
+	private int days;
+	
+	//constructs special subclass with new field and invariant validation
+	public StreakQuest(int id, String title, int basePoints, int days) {
+		super(id, title, basePoints);
+		
+		if(days<=0) {
+			throw new IllegalArgumentException("Days must be greater than 0");
+		}
+		
+		this.days=days;
+	}
+	
+	
+	//overrides method to add points based off days (2x days + base points)
+	@Override
+	public int completeFor(Student s) {
+		int finalPoints = getBasePoints() + 2*days;
+		
+		s.addPoints(finalPoints);
+		
+		return finalPoints;
+	}
+	
 }
