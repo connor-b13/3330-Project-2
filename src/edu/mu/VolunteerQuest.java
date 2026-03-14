eb962adcdc356d2b4146aa76cb39267288e4e5ef
Connor Bailey <connorwb2005@gmail.com>
2026-03-14 17:41:58 -0500

    Implemented VolunteerQuest

--- src/edu/mu/VolunteerQuest.java
+++ src/edu/mu/VolunteerQuest.java
@@ -1,5 +1,25 @@
 package edu.mu;
 
-public class VolunteerQuest {
+public class VolunteerQuest extends AbstractQuest {
 
+	int hours;
+	
+	//constructs special subclass with new field and invariant validation
+	public VolunteerQuest(int id, String title, int basePoints, int hours) {
+		if(hours<=0) {
+			throw new IllegalArgumentException("Must have at least 1 hour");
+		}
+		super(id, title, basePoints);
+		this.hours=hours;
+	}
+
+	//overrides method to add points multiplied by volunteer hours
+	@Override
+	public int completeFor(Student s) {
+		int finalPoints = getBasePoints()*hours;
+		
+		s.addPoints(finalPoints);
+		
+		return finalPoints;
+	}
 }
