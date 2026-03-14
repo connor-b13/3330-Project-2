01230d453f8007db4db11a483093bdf24f042a8a
Connor Bailey <connorwb2005@gmail.com>
2026-03-14 17:40:32 -0500

    Implemented EventCheckInQuest

--- src/edu/mu/EventCheckInQuest.java
+++ src/edu/mu/EventCheckInQuest.java
@@ -1,5 +1,23 @@
 package edu.mu;
 
-public class EventCheckInQuest {
+public class EventCheckInQuest extends AbstractQuest{
+	
+	private String eventName;
 
+	
+	//constructs special subclass with new field and invariant validation
+	public EventCheckInQuest(int id, String title, int basePoints, String eventName) {
+		if (eventName == null||eventName.isEmpty()) {
+			throw new IllegalArgumentException("Must include a valid event name.");
+		}
+		super(id, title, basePoints);
+		this.eventName=eventName;
+		
+	}
+
+//Simple getter for new field
+	public String getEventName() {
+		return eventName;
+	}
+	
 }
